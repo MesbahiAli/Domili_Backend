0a88b3b30b03d58e5259000b5cf3a3524a33a51a
iliass bargache <ibargache6@gmail.com>
2025-02-01 02:40:41 +0100

    cindexa afichage + provider details

--- src/main/java/com/example/domily/entity/User.java
+++ src/main/java/com/example/domily/entity/User.java
@@ -13,127 +13,147 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
+    private int age ;
+    
     private String nom;
     private String prenom;
     private String phone ;
     private String Secteur;
     private String about ;
 
-   
+
+    
+    private String sexe ;
+    
+    
+    
     @Column(unique = true, nullable = false)
     private String email;
-
+    
     private String password;
-
+    
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
+    
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference("user-order")
     @JsonIgnoreProperties("user") // Prevent serialization of the "user" field in Order
     private List<Order> orders = new ArrayList<>();
-
-
-
+    
+    
+    
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference("user-review")
     @JsonIgnoreProperties("user") // Prevent serialization of the "user" field in Review
     private List<Review> reviews = new ArrayList<>();
-
-
+    
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getNom() {
         return nom;
     }
-
+    
     public void setNom(String nom) {
         this.nom = nom;
     }
-
+    
     public String getPrenom() {
         return prenom;
     }
-
+    
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
+    
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public Role getRole() {
         return role;
     }
-
+    
     public void setRole(Role role) {
         this.role = role;
     }
-
+    
     public List<Order> getOrders() {
         return orders;
     }
-
+    
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
-
+    
     public List<Review> getReviews() {
         return reviews;
     }
-
+    
     public void setReviews(List<Review> reviews) {
         this.reviews = reviews;
     }
-        
+    
     public String getPhone() {
         return phone;
     }
-
+    
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
+    
     public String getSecteur() {
         return Secteur;
     }
-
+    
     public void setSecteur(String secteur) {
         Secteur = secteur;
     }
-
+    
     public String getAbout() {
         return about;
     }
-
+    
     public void setAbout(String about) {
         this.about = about;
     }
-
-
     
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getSexe() {
+        return sexe;
+    }
+    
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
 }
-   
 
-  
+
